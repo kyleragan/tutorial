de1d76c0c8d12e532c73890c8061ea6e9abe5c1e
Kyle Ragan <kyle.ragan@gmail.com>
2011-01-30 04:23:31 -0600

    added help page and tests. RED

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -46,7 +46,23 @@ describe PagesController do
 					
 	end
 
-end
+	end
+
+  describe "GET 'help'" do
+	it "should be successful" do
+		get 'help'
+		response.should be_success
+	end
+	
+	it "should have the right title" do
+		get 'help'
+		response.should have_selector("title",
+					:content => "Ruby on Rails Tutorial App | Help")
+					
+	end
+
+	end
+  
   
 
 end
