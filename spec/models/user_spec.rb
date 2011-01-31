988eb82887f97db187e8ba5cc428317a310d2468
Kyle Ragan <kyle.ragan@gmail.com>
2011-01-31 13:20:58 -0600

    GREEN. validates presence of name and email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,13 @@ describe User do
 		User.create!(@attr)
 	end
 	
-	it "should require a name"
+	it "should require a name" do
+		no_name_user = User.new(@attr.merge(:name => ""))
+		no_name_user.should_not be_valid
+	end
+	
+	it "should require an email address" do
+		no_email_user = User.new(@attr.merge(:email => ""))
+		no_email_user.should_not be_valid
+	end
 end
