21b2ea2aed220ddf8d8accbcf4121ce98d3df398
Kyle Ragan <kyle.ragan@gmail.com>
2011-01-31 13:43:35 -0600

    GREEN. validates name and email further

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,4 +19,39 @@ describe User do
 		no_email_user = User.new(@attr.merge(:email => ""))
 		no_email_user.should_not be_valid
 	end
+	
+	it "should reject names that are too long" do
+		long_name = "a"*51
+		long_name_user = User.new(@attr.merge(:name => long_name))
+		long_name_user.should_not be_valid
+	end
+	
+	it "should accept valid email addresses" do
+		addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+		addresses.each do |address|
+			valid_email_user = User.new(@attr.merge(:email => address))
+			valid_email_user.should be_valid
+		end
+	end
+	
+	it "should reject invalid email addresses" do
+		addresses = %w[user@foo,com user_at_foo.com first.last@example.]
+		addresses.each do |address|
+			invalid_email_user = User.new(@attr.merge(:email => address))
+			invalid_email_user.should_not be_valid
+		end
+	end
+	
+	it "should reject duplicate email addresses" do
+		User.create!(@attr)
+		duplicate_email_user = User.new(@attr)
+		duplicate_email_user.should_not be_valid
+	end
+	
+	it "should reject email addresses identical up to case" do
+		User.create!(@attr.merge(:email => @attr[:email].upcase))
+		duplicate_email_user = User.new(@attr)
+		duplicate_email_user.should_not be_valid
+	end
+		
 end
