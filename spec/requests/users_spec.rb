98808c03ce022bbd1523bbd92f0a21c163b600b3
Kyle Ragan <kyle.ragan@gmail.com>
2011-02-02 20:33:11 -0600

    GREEN. sign in/out works.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -39,4 +39,28 @@ describe "Users" do
 		end
 		
 	end
+	
+	describe "signin/out" do
+		
+		it "should fail" do
+			visit signin_path
+			fill_in :email, 			:with => ""
+			fill_in :password, 		:with => ""
+			click_button
+			response.should have_selector("div.flash.error", :content => "Invalid")
+		end
+		
+		it "should succeed" do
+			user = Factory(:user)
+			visit signin_path
+			fill_in :email, 			:with => user.email
+			fill_in :password, 		:with => user.password
+			click_button
+			controller.should be_signed_in
+			click_link "Sign out"
+			controller.should_not be_signed_in
+		end
+		
+	end
+	
 end
